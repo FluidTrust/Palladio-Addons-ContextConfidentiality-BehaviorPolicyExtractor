2ae17340e4d92ebc6d72e23285f6c5cbad6a95c9
Thomas <thomas.lieb91@gmail.com>
2020-11-11 10:59:19 +0100

    Create tests for evaluation

---
+++ tests/org.palladiosimulator.pcm.confidentiality.context.policyextractor.tests/src/policyextractor/tests/accuracy/AccuracyErrorTest.java
@@ -0,0 +1,15 @@
+package policyextractor.tests.accuracy;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+class AccuracyErrorTest {
+
+    @Test
+    void test1() throws IOException {
+        // PalladioModelGenerator pmg = new PalladioModelGenerator();
+        // pmg.saveTestModels();
+    }
+
+}
